73ca33c0d66b6d263d996dc37c33ff2a52565268
Noah Hoffman <noah.hoffman@gmail.com>
2011-04-17 22:17:44 -0700

    - uses __version__ in Menu module - changed url to github repo - formatting

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ Create unix package: python setup.py sdist
 Create windows package: python setup.py bdist_wininst
 """
 
-revision = '$Revision$'
+from Menu import __version__
 
 from distutils.core import setup
-setup(name='Menu',
-      version=revision[:-1].split(':')[1].strip(),
-      py_modules=['Menu'],
-      description='Creates a simple, interactive text-based user interface',
-      author='Noah Hoffman',
-      author_email='noah.hoffman@gmail.com',
-      url='http://staff.washington.edu/ngh2',
+setup(name = 'Menu',
+      version = __version__,
+      py_modules = ['Menu'],
+      description = 'Creates a simple, interactive text-based user interface',
+      author = 'Noah Hoffman',
+      author_email = 'noah.hoffman@gmail.com',
+      url = 'https://github.com/nhoffman/pymenu',
       )
